9be07b2f9a5f37628b628235172bad32f1bba56d
Piotr Chmielowiec <chpiotr06@gmail.com>
2022-10-19 14:11:09 +0200

    Added enter button functionality, Link elements, minor responsiveness

--- src/pages/LoginPage/LoginForm.jsx
+++ src/pages/LoginPage/LoginForm.jsx
@@ -1,16 +1,32 @@
 import css from './LoginForm.module.scss';
 import Panel from "../../components/Panel";
 import PropTypes from "prop-types";
+import { useRef, useState }  from 'react';
+import { Link } from 'react-router-dom';
 
 function LoginForm({onLoginAttempt}) {
 
-  const clickHandler = event => {
-    event.preventDefault();
+  const passwordElement = useRef(null);
+  const [ password, setPassword ] = useState('');
+  const [ email, setEmail ] = useState('');
+
+  const sendAttempt = () => {
+    setPassword('');
+    setEmail('');
   }
 
-  /**
-   * TODO: implement valid focusing, implement design for smaller devices 
-   * TODO: add routes for new account and forgotten password
+  const handleMail = event => {
+    if(event.key === 'Enter'){
+      passwordElement.current.focus();
+    }
+  }
+  const handlePassword = (event) => {
+    if(event.key === 'Enter'){
+      sendAttempt();
+    }
+  }
+
+  /** 
    * TODO: add code for displaying login errors
    */
 
@@ -20,18 +36,46 @@ function LoginForm({onLoginAttempt}) {
         <fieldset>
           <legend>Log in</legend>
           <label className={css.emailLabel} htmlFor="email">Email</label>
-          <input type="text" id="email" placeholder='Enter email...'/>
+
+          <input 
+            type="text" 
+            id="email" 
+            placeholder='Enter email...' 
+            value={email} 
+            onChange={event => setEmail(event.target.value)}
+            onKeyDown={handleMail} 
+          />
+
           <div className={css.flexRowSpaceBetween}>
             <label htmlFor="password">Password</label>
-            <span className={css.blueUnderline}>Forgot your password?</span>
+            <div className={css.blueUnderline}>
+              <Link to="/password-recovery" className={css.blueUnderline}>Forgot your password?</Link>
+            </div>
           </div>
-          <input type="password" id="password" placeholder='Enter password...'/>
-          <input type="submit" id="submit" value="Log in" onClick={clickHandler}/>
+
+          <input 
+            type="password" 
+            id="password" 
+            placeholder='Enter password...' 
+            ref={passwordElement} 
+            value={password} 
+            onChange={event => setPassword(event.target.value)} 
+            onKeyDown={handlePassword}
+          />
+
+          <input 
+            type="submit" 
+            id="submit" 
+            value="Log in" 
+            onClick={event => event.preventDefault()} 
+            onMouseDown={sendAttempt}
+          />
+
         </fieldset>
       </form>
       <div className={css.newAccountSugestion}>
-        <span className={css.noAccount}>Don't have an account yet?</span>
-        <span className={css.blueUnderline}>Create an account</span>
+        <div className={css.noAccount}>Don't have an account yet?</div>
+        <div className={css.blueUnderline}><Link to="/new-account" className={css.blueUnderline}>Create an account</Link></div>
       </div>
     </Panel>
   )
